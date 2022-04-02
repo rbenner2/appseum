7ab755aeed8f21b0d580b3f8a39b233ce603fdd6
Regan Benner <regan.benner@hotmail.com>
2022-04-02 00:05:20 -0400

    git

--- back-end/index.js
+++ back-end/index.js
@@ -37,7 +37,15 @@ app.use(bodyParser.json({
 app.use(cookieParser());
 
 
+const path = require("path");
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../front-end/build")));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../front-end/build/index.html"));
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/items", itemRouter);
